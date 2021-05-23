16d81ee6d00bd21a07c4e3fb58129a3925534715
jun-yub-kim <81306023+jun-yub-kim@users.noreply.github.com>
2021-05-23 17:44:49 +0900

    Update shell.c

--- shell.c
+++ shell.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <dirent.h> // chdir 을 위한 헤더파일 (cd 명령어를 만들기 위함)
 #include <sys/wait.h>
 
 #define MAX_LEN_LINE    100
@@ -15,7 +16,9 @@ int main(void)
     int ret, status;
     pid_t pid, cpid;
     
-    printf("종료키는 'exit' 입니다"
+    printf("안녕하세요 제가만든 shell을 사용해주셔서 감사합니다. \n");
+    printf("종료키는 'exit' 입니다\n");
+    printf("명령어 목록키는 'help' 입니다.\n\n\n\n\n");
     
     while (true) {
         char *s;
@@ -23,10 +26,7 @@ int main(void)
         
         printf("MyShell $ ");
         s = fgets(command, MAX_LEN_LINE, stdin);
-        
-        if (s == "exit") {
-            exit(0)
-        }
+
         
         if (s == NULL) {
             fprintf(stderr, "fgets failed\n");
@@ -34,35 +34,89 @@ int main(void)
         }
         
         len = strlen(command);
-        printf("%d\n", len);
+        
         if (command[len - 1] == '\n') {
             command[len - 1] = '\0'; 
         }
+
         
-        printf("[%s]\n", command);
+        if (strcmp(command, "exit") == 0) {
+                printf("프로그램 종료합니다.\n");
+                return(-1);
+            }
+
+        
       
         pid = fork();
-        if (pid < 0) {
+
+
+
+        if (pid < 0) { //pid 는 음수
             fprintf(stderr, "fork failed\n");
             exit(1);
-        } 
-        if (pid != 0) {  /* parent */
+        }
+
+        if (pid != 0) {  /* parent */ // pid 는 양수
+            
             cpid = waitpid(pid, &status, 0);
+
             if (cpid != pid) {
                 fprintf(stderr, "waitpid failed\n");        
             }
+
             printf("Child process terminated\n");
+
             if (WIFEXITED(status)) {
                 printf("Exit status is %d\n", WEXITSTATUS(status)); 
             }
         }
-        else {  /* child */
+
+
+
+        else {  /* child */ //pid 는 0
+
+
+            if (strcmp(s, "help") == 0) {
+                printf("명령어 : ls / 오늘의 학식 / 오늘의 날씨 / 눈뽕\n");
+                return 0;
+            }
+
+            if (strcmp(s, "오늘의 학식") == 0){
+                system("explorer.exe http://www.kookmin.ac.kr/user/unLvlh/lvlhSpor/todayMenu/index.do");
+                exit(0);
+            }
+
+            if (strcmp(s, "오늘의 날씨") == 0){
+                system("explorer.exe https://www.weather.go.kr/w/weather/forecast/short-term.do#");
+                exit(0);
+            }
+
+            if (strcmp(s, "눈뽕") == 0){
+                for (int i = 0; i < 50; i ++){
+                     int c = rand()% 7 + 1;
+                     usleep(50000);
+                     printf("\033[4%dm                                                                                                                                                                                                                                                                                                                                                                                               \n", c);
+                     }
+                printf("\033[0m \n\n\n눈건강은 중요합니다.\n");
+                exit(0);
+            }
+
+            if (strcmp(s, "ls")== 0){
+		        args[0] = "/bin/ls";
+	        }
+
+            
+
+
+
             ret = execve(args[0], args, NULL);
             if (ret < 0) {
                 fprintf(stderr, "execve failed\n");   
                 return 1;
             }
-        } 
+        }
+
     }
     return 0;
 }
+
